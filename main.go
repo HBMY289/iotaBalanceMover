44250b82b791b4d8672d1647b64fb3f927af139e
HBMY289 <HBMY289@gmail.com>
2021-03-07 13:41:15 +0100

    introduced state and highlight spent addrs

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 )
 
 var iotaAPI *api.API
+var acc accountState
 
 const defaultNode = "https://nodes.iota.org:443"
 const seedLen = 81
@@ -20,12 +21,12 @@ const addrsPerBatch = 500
 func main() {
 	fmt.Println("\nWelcome!\nThis program will list all addresses of your seed with a positive balance and will let you move the funds of a specific address.")
 	getAPI()
-	seed := getSeed()
+	getSeed()
 
 	for {
-		addrs, balances := getAddressesWithBalance(seed)
-		printAddrWithBalance(addrs, balances)
-		moveBalance(seed, addrs, balances)
+		getAccountState()
+		printAccountState()
+		moveBalance()
 		fmt.Print("\nDo you want to move funds of another address of this seed? (y/n): ")
 		var answer string
 		fmt.Scanln(&answer)
@@ -36,7 +37,6 @@ func main() {
 }
 
 func getAPI() {
-
 	nodeURL := defaultNode
 	for {
 		var err error
@@ -56,16 +56,15 @@ func getAltNodeURL(oldNode string) string {
 	return URL
 }
 
-func getSeed() string {
-	var seed string
-	var answer string
+func getSeed() {
+	var seed, answer string
 	for {
 		fmt.Print("\nEnter seed: ")
 		fmt.Scanln(&seed)
 		if !hasInvalidChars(seed) {
 			if !hasInvalidChars(seed) {
 				if len(seed) == seedLen {
-					return seed
+					break
 				}
 				if len(seed) < seedLen {
 					fmt.Printf("The seed has less than %d characters. Do you want to continue with this seed? (y/n): ", seedLen)
@@ -78,15 +77,14 @@ func getSeed() string {
 				fmt.Scanln(&answer)
 				fmt.Println()
 				if answer == "y" {
-					return seed
+					break
 				}
 			}
 		} else {
 			fmt.Println("\nValid seeds only contain upper case letters A-Z and the number 9.")
 		}
 	}
-
-	return seed
+	acc.seed = seed
 }
 
 func hasInvalidChars(seed string) bool {
@@ -99,22 +97,19 @@ func hasInvalidChars(seed string) bool {
 
 }
 
-func getAddressesWithBalance(seed string) ([]string, []uint64) {
-	var addrs []string
-	var balances []uint64
-	var total uint64
+func getAccountState() {
+
 	var answer string
 	index := 0
-
+	acc.resetState()
 	for {
 		fmt.Printf("\nGenerating addresses #%d to #%d\n", index, index+addrsPerBatch)
-		addrBatch := generateAddresses(seed, index, addrsPerBatch)
-		addrs = append(addrs, addrBatch...)
+		addrBatch := generateAddresses(index, addrsPerBatch)
+		acc.addrs = append(acc.addrs, addrBatch...)
 		balancesBatch := getBalances(addrBatch)
-		balances = append(balances, balancesBatch...)
-		total += getSumBalance(balancesBatch)
-		if total > 0 {
-			fmt.Printf("\nFound a total of %di on the first %d addresses.\nIs the total balance correct? (y/n):", total, index+addrsPerBatch)
+		acc.balances = append(acc.balances, balancesBatch...)
+		if acc.totalBalance() > 0 {
+			fmt.Printf("\nFound a total of %di on the first %d addresses.\nIs the total balance correct? (y/n):", acc.totalBalance(), index+addrsPerBatch)
 			fmt.Scanln(&answer)
 			if answer == "y" {
 				fmt.Println()
@@ -123,26 +118,14 @@ func getAddressesWithBalance(seed string) ([]string, []uint64) {
 		}
 		index += addrsPerBatch
 	}
-	return addrs, balances
-
+	getSpentStates()
 }
 
-func getSumBalance(balances []uint64) uint64 {
-	var sum uint64
-	for _, b := range balances {
-		sum += b
-	}
-	return sum
-}
-
-func generateAddresses(seed string, start, count int) []string {
+func generateAddresses(start, count int) []string {
 	var addrs []string
-	for i := 0; i < count; i++ {
-		addr, err := address.GenerateAddress(seed, uint64(start+i), consts.SecurityLevelMedium, true)
-		if err != nil {
-			panic(err)
-		}
-		addrs = append(addrs, addr)
+	addrs, err := address.GenerateAddresses(acc.seed, uint64(start), uint64(count), consts.SecurityLevelMedium, true)
+	if err != nil {
+		panic(err)
 	}
 	return addrs
 }
@@ -150,33 +133,47 @@ func generateAddresses(seed string, start, count int) []string {
 func getBalances(addrs []string) []uint64 {
 	balances, err := iotaAPI.GetBalances(addrs)
 	if err != nil {
-		// handle error
 		panic(err)
 	}
 	return balances.Balances
 }
 
-func printAddrWithBalance(addrs []string, balances []uint64) {
-	var total uint64
-	for i, b := range balances {
-		if b > 0 {
-			fmt.Printf("address #%d: %di (%s)\n", i, b, addrs[i])
-			total += b
-
-		}
-
-	}
-	fmt.Printf("Total balance: %di\n", total)
-}
-
-func moveBalance(seed string, addrs []string, balances []uint64) {
-	var confirm string
-	index := getIndex(addrs, balances)
-	used, err:=iotaAPI.WereAddressesSpentFrom(addrs[index])
+func getSpentStates() {
+	spentStates, err := iotaAPI.WereAddressesSpentFrom(acc.addrs...)
 	if err != nil {
 		panic(err)
 	}
-	if used[0] {
+	acc.spent = spentStates
+}
+
+func printAccountState() {
+	fmt.Println("Listing all addresses with a positive balance:")
+	for i, addr := range acc.addrs {
+		balance := strconv.Itoa(int(acc.balances[i])) + "i"
+		if acc.balances[i] > 0 {
+			if acc.spent[i] {
+				balance = inRed("(" + balance + ")")
+			}
+			fmt.Printf("address #%d: %s %s\n", i, balance, addr)
+		}
+	}
+	fmt.Printf("Total balance: %di\n", acc.totalBalance())
+	if acc.fundsOnSpentAddr() {
+		fmt.Printf("Funds on spent addresses are shown in %s.\n", inRed("( )"))
+	}
+}
+
+func inRed(text string) string {
+	colorRed := "\033[31m"
+	colorReset := "\033[0m"
+	return string(colorRed) + text + string(colorReset)
+}
+
+func moveBalance() {
+	var confirm string
+	index := getChosenIndex()
+
+	if acc.spent[index] {
 		fmt.Printf("WARNING!!!\nThe chosen address was already used for spending.\nSending multiple times from the same address can put these funds at risk.\nAre you sure you want to proceed? (y/n):")
 		fmt.Scanln(&confirm)
 		if confirm != "y" {
@@ -184,80 +181,106 @@ func moveBalance(seed string, addrs []string, balances []uint64) {
 		}
 	}
 	target := getTargetAddress()
-	fmt.Printf("\n\nMoving %di from address \n%s\nto address\n%s\nDo you want to proceed? (y/n):",balances[index],addrs[index],target)
-	
+	fmt.Printf("\n\nMoving %di from address \n%s\nto address\n%s\nDo you want to proceed? (y/n):", acc.balances[index], acc.addrs[index], target)
+
 	fmt.Scanln(&confirm)
 	if confirm == "y" {
 		fmt.Println("\nSending transaction")
-		sendBalance(seed, addrs, balances, index, target)
+		sendBalance(index, target)
 	}
-	
+
 }
 
-
-
-
-func getIndex(addrs []string, balances []uint64) int {
+func getChosenIndex() int {
 	var index int
 	for {
 		var input string
-		fmt.Print("You can move the funds of an address by entering its index number.\nAddress index:")
+		fmt.Print("\nYou can move the funds of an address by entering its index number.\nAddress index:")
 		fmt.Scanln(&input)
 		var err error
 		index, err = strconv.Atoi(input)
 		if err != nil {
-			fmt.Println("Only numerical input is allowed.\n")
-		} else{
-			if balances[index] >0 {
+			fmt.Println("Only numerical input is allowed.")
+		} else {
+			if acc.balances[index] > 0 {
 				break
 			}
 			fmt.Printf("Address #%d does not have a balance.\n", index)
 		}
-		
+
 	}
 	return index
 }
 
-
-func getTargetAddress () string {
+func getTargetAddress() string {
 	var addr string
 	for {
 		fmt.Print("Enter the address you want to move the funds to: ")
-		fmt.Scanln (&addr)
+		fmt.Scanln(&addr)
 		if address.ValidAddress(addr) == nil {
 			break
 		}
 		fmt.Println("Please enter valid iota address with checksum (90 characters).")
 	}
-	return addr	
+	return addr
 }
-func sendBalance(seed string, addrs []string, balances []uint64, i int, target string) {
+func sendBalance(i int, target string) {
 
 	transfers := bundle.Transfers{
 		{
 			Address: target,
-			Value:   balances[i],
+			Value:   acc.balances[i],
 		},
 	}
 	inputs := []api.Input{
 		{
 
-			Address:  addrs[i],
+			Address:  acc.addrs[i],
 			Security: consts.SecurityLevelMedium,
 			KeyIndex: uint64(i),
-			Balance:  balances[i],
+			Balance:  acc.balances[i],
 		},
 	}
 	prepTransferOpts := api.PrepareTransfersOptions{Inputs: inputs}
 
-	trytes, err := iotaAPI.PrepareTransfers(seed, transfers, prepTransferOpts)
+	trytes, err := iotaAPI.PrepareTransfers(acc.seed, transfers, prepTransferOpts)
 	if err != nil {
 		panic(err)
 	}
-	b,err := iotaAPI.SendTrytes (trytes,2,14)
+	b, err := iotaAPI.SendTrytes(trytes, 2, 14)
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Printf("\nSuccessfully sent transaction:\n%s\n", bundle.TailTransactionHash(b))
+}
+
+func (s *accountState) resetState() {
+	s.addrs = nil
+	s.balances = nil
+	s.spent = nil
+}
+
+func (s *accountState) fundsOnSpentAddr() bool {
+	for i, s := range s.spent {
+		if s && acc.balances[i] > 0 {
+			return true
+		}
+	}
+	return false
+}
+
+func (s *accountState) totalBalance() uint64 {
+	var sum uint64
+	for _, b := range s.balances {
+		sum += b
+	}
+	return sum
+}
+
+type accountState struct {
+	seed     string
+	addrs    []string
+	balances []uint64
+	spent    []bool
 }
